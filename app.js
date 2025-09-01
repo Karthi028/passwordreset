66242a43551a54d8b614ee865ceeaff012fa5878
karthijayasangar <karthijayasangar6564@gmail.com>
2025-09-02 02:55:25 +0530

    cors updated

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const cookieParser = require('cookie-parser');
 const app = express();
 
 const corsOptions = {
-  origin: 'http://localhost:5174', 
+  origin: 'https://passwordreset-dnbd.onrender.com', 
   credentials: true, 
 };
 
